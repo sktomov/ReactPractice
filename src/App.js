6663cab604fcffcc90a29ae6ea427e8a46ed94dc
le6o <sktomov@gmail.com>
2018-06-11 20:48:09 +0300

    css futures

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import classes from './App.css';
 import Person from './Person/Person';
 
 
@@ -65,18 +65,18 @@ class App extends Component {
       style.backgroundColor = 'red';
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2){
-      classes.push('red');
+      assignedClasses.push( classes.red );
     }
     if(this.state.persons.length <= 1){
-      classes.push('bold');
+      assignedClasses.push( classes.bold );
     }
 
     return (
-        <div className="App">
+        <div className={classes.App}>
           <h1>Hi I'm React app</h1>
-          <p className={classes.join(' ')}>Cool</p>
+          <p className={assignedClasses.join(' ')}>Cool</p>
           <button style={style}
            onClick={this.showNamesHandler}>Toggle Name</button>
            {persons}
